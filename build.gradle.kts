755b50c519557abfd7a7ebe98d48e55ebcb99783
Dejan Igrec <dejan.igrec@gmail.com>
2024-07-23 22:02:39 +0200

    chore: rename Compose Compiler plugin alias

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,6 @@ plugins {
     alias(libs.plugins.androidApplication) apply false
     alias(libs.plugins.androidLibrary) apply false
     alias(libs.plugins.jetbrainsCompose) apply false
-    alias(libs.plugins.compose.compiler) apply false
+    alias(libs.plugins.composeCompiler) apply false
     alias(libs.plugins.kotlinMultiplatform) apply false
 }
